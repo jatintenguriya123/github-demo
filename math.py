251e09209b0e930216c020a61c6ea3fa585fb332
Jatin Tenguriya <63942434+jatintenguriya123@users.noreply.github.com>
2021-07-05 15:54:30 +0530

    Add files via upload
    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implementation
+def add(x,y):
+    return x+y
+
+#subtract implementation   
+def subtract(x,y):
+    return x-y #sub implementation
+
+#multiply implementation    
+def multiply(x,y):
+    return x*y #on master branch
+      
+#divide implementation      
+def divide(x,y):
+
+    if y==0:
+        return divideby0error
+    else:
+        return x/y #on bug789
+
+def square (x):
+    pass
